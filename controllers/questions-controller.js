0e7f75b21ad4cdf0eb7ef4d156b0ad2af0cd88a4
vishwasthakkar <thakkar.vi@husky.neu.edu>
2021-04-08 20:45:30 -0400

    Updated controllers

--- controllers/questions-controller.js
+++ controllers/questions-controller.js
@@ -3,7 +3,7 @@ const questionsService = require('../services/questions-service');
 module.exports = (app) => {
 
     const getQuestionsForQuiz = (req, res) =>
-        res.send(questionsService.findQuestionsForQuiz(req.params.quizId));
+        res.json(questionsService.findQuestionsForQuiz(req.params.quizId));
 
     app.get('/api/quizzes/:quizId/questions', getQuestionsForQuiz);
 
--- controllers/quizzes-controller.js
+++ controllers/quizzes-controller.js
@@ -3,10 +3,10 @@ const quizService = require('../services/quiz-service');
 module.exports = app => {
 
     const getAllQuizzes = (req, res) =>
-        res.send(quizService.findAllQuizzes());
+        res.json(quizService.findAllQuizzes());
 
     const getQuizById = (req, res) =>
-        res.send(quizService.findQuizById(req.params.quizId));
+        res.json(quizService.findQuizById(req.params.quizId));
 
     app.get('/api/quizzes', getAllQuizzes);
     app.get('/api/quizzes/:quizId', getQuizById);
