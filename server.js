ea63f8cd9952bf0eb8aa55aea0b7e59c5fbaf26c
vishwasthakkar <thakkar.vi@husky.neu.edu>
2021-04-18 00:11:21 -0400

    Conifgured port

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ require('./controllers/quizzes-controller')(app);
 require('./controllers/questions-controller')(app);
 require('./controllers/quiz-attempts-controller')(app);
 
-app.listen(process.env.port || 80);
+app.listen(process.env.port || 443);
