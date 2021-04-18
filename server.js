170b9d87e1f1ac84f2a3b3c1bbb6cb80a4ec42a3
vishwasthakkar <thakkar.vi@husky.neu.edu>
2021-04-18 00:13:42 -0400

    Connected to remote db

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ require('./controllers/quizzes-controller')(app);
 require('./controllers/questions-controller')(app);
 require('./controllers/quiz-attempts-controller')(app);
 
-app.listen(process.env.port || 443);
+app.listen(process.env.PORT || 4000);
